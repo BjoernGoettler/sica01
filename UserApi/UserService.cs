6de2a51b1a8bbbdb604b8a905715d96413740de2
Bjørn Göttler <bjoern@goettler.dk>
2024-09-27 18:15:19 +0200

    :truck: Rename service

---
+++ UserApi/UserService.cs
@@ -0,0 +1,27 @@
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+using UserApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddDbContext<UserContext>(opt=>opt.UseInMemoryDatabase("Users"));
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.MapControllers();
+app.UseHttpsRedirection();
+
+app.Run();
+Log.CloseAndFlush();
